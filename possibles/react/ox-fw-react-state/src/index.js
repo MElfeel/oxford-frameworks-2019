880c6e7ef62eef03f14883168f1f75a82547f375
John Coumbe <john.coumbe@gmail.com>
2019-05-29 16:47:37 +0100

    Remove React Possibles Examples

--- possibles/react/ox-fw-react-state/src/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Shop from './Shop';
-import './style.css';
-
-let basket = [ "Cheese","Ham","Bread","Milk" ];
-
-render(<Shop stock={basket} />, document.getElementById('root'));
