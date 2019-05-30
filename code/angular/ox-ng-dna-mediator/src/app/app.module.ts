85df0d89c8a5276e49cf2465dd0bdffdb0205001
John Coumbe <john.coumbe@gmail.com>
2019-05-30 12:31:18 +0100

    restructuring

--- code/angular/ox-ng-dna-mediator/src/app/app.module.ts
+++ code/angular/ox-ng-dna-mediator/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { BaseComponent } from './base/base.component';
 import { DrawComponent } from './draw/draw.component';
 import { MessageService } from './services/message.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, BaseComponent, DrawComponent ],
+  declarations: [ AppComponent, BaseComponent, DrawComponent ],
   bootstrap:    [ AppComponent ],
   providers: [MessageService]
 })
